8157cd1acfbe591ea5551e7e2669673290113219
retoburri95 <retoburritest@gmail.com>
2025-06-13 16:51:57 +0200

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello Azure"
+
+@app.route('/add', methods=['GET'])
+def add():
+    try:
+        a = float(request.args.get('a', 0))
+        b = float(request.args.get('b', 0))
+        result = a + b
+        return jsonify({'result': result})
+    except (TypeError, ValueError):
+        return jsonify({'error': 'Invalid input'}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)    
